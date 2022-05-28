de6ab8fa870513a85e25e75d08f8d5f34e7b0bf8
Silverlan <silverlann@gmail.com>
2022-05-28 15:42:06 +0200

    Fix camera spot-light control not applying properties correctly

--- lua/entities/components/pfm_camera_actor_link/client/cl_init.lua
+++ lua/entities/components/pfm_camera_actor_link/client/cl_init.lua
@@ -24,12 +24,19 @@ end
 function Component:SetTargetActor(actor) self.m_targetActor = actor end
 function Component:OnPoseChanged()
 	if(util.is_valid(self.m_targetActor) == false) then return end
-	self.m_targetActor:SetPose(self:GetEntity():GetPose())
+	local pm = tool.get_filmmaker()
+	local actorC = self.m_targetActor:GetComponent(ents.COMPONENT_PFM_ACTOR)
+	if(actorC == nil) then return end
+	local pose = self:GetEntity():GetPose()
+	self.m_targetActor:SetPose(pose)
+	pm:SetActorTransformProperty(actorC,"position",pose:GetOrigin())
+	pm:SetActorTransformProperty(actorC,"rotation",pose:GetRotation())
 	local camC = self:GetEntity():GetComponent(ents.COMPONENT_CAMERA)
 	if(camC == nil) then return end
 	local lightC = self.m_targetActor:GetComponent(ents.COMPONENT_LIGHT_SPOT)
 	if(lightC ~= nil) then
 		lightC:SetOuterConeAngle(camC:GetFOV())
+		pm:SetActorGenericProperty(actorC,"ec/light_spot/outerConeAngle",camC:GetFOV())
 	end
 end
 ents.COMPONENT_PFM_CAMERA_ACTOR_LINK = ents.register_component("pfm_camera_actor_link",Component)
