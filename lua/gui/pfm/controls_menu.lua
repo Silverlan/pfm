2d00d508de110aa8feca80df1ff237ea402b2f59
Silverlan <silverlann@gmail.com>
2023-03-07 20:50:32 +0100

    fix: add missing include

--- lua/gui/pfm/controls_menu.lua
+++ lua/gui/pfm/controls_menu.lua
@@ -8,6 +8,7 @@
 
 include("/gui/vbox.lua")
 include("/gui/pfm/slider.lua")
+include("/gui/fileentry.lua")
 
 util.register_class("gui.PFMControlsMenu",gui.VBox)
 function gui.PFMControlsMenu:__init()
