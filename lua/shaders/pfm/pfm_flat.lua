bb42060a7ead55403ed2431fddf29f49032ef2b1
Silverlan <silverlann@gmail.com>
2023-04-11 14:00:23 +0200

    feat: add shader pfm_flat_wdt

--- lua/shaders/pfm/pfm_flat.lua
+++ lua/shaders/pfm/pfm_flat.lua
@@ -1,13 +1,21 @@
-util.register_class("shader.PFMFlat",shader.BaseTexturedLit3D)
+local Shader = util.register_class("shader.PFMFlat",shader.BaseTexturedLit3D)
 
-shader.PFMFlat.FragmentShader = "pfm/flat/fs_flat"
-shader.PFMFlat.VertexShader = "pfm/flat/vs_flat"
-function shader.PFMFlat:__init()
+Shader.FragmentShader = "pfm/flat/fs_flat"
+Shader.VertexShader = "pfm/flat/vs_flat"
+function Shader:__init()
 	shader.BaseTexturedLit3D.__init(self)
 end
-function shader.PFMFlat:InitializePipeline(pipelineInfo,pipelineIdx)
+function Shader:InitializePipeline(pipelineInfo,pipelineIdx)
 	shader.BaseGraphics.InitializePipeline(self,pipelineInfo,pipelineIdx)
 
 	pipelineInfo:SetDepthTestEnabled(false)
 end
-shader.register("pfm_flat",shader.PFMFlat)
+shader.register("pfm_flat",Shader)
+
+local ShaderWdt = util.register_class("shader.PFMFlatWdt",Shader)
+function ShaderWdt:InitializePipeline(pipelineInfo,pipelineIdx)
+	Shader.InitializePipeline(self,pipelineInfo,pipelineIdx)
+
+	pipelineInfo:SetDepthTestEnabled(true)
+end
+shader.register("pfm_flat_wdt",ShaderWdt)
