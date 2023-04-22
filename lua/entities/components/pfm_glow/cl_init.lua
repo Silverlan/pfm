a6d2b8695218ea2c71314cb7a39f82b47b25e354
Silverlan <silverlann@gmail.com>
2023-04-22 10:40:37 +0200

    feat: add pfm_glow component

--- lua/entities/components/pfm_glow/cl_init.lua
+++ lua/entities/components/pfm_glow/cl_init.lua
@@ -6,7 +6,7 @@
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
 ]]
 
-local Component = util.register_class("ents.PFMBloom",BaseEntityComponent)
+local Component = util.register_class("ents.PFMGlow",BaseEntityComponent)
 
 Component:RegisterMember("BlurRadius",udm.TYPE_UINT32,6,{
 	onChange = function(self)
@@ -16,13 +16,6 @@ Component:RegisterMember("BlurRadius",udm.TYPE_UINT32,6,{
     max = 15
 })
 Component:RegisterMember("BlurSigma",udm.TYPE_FLOAT,10.0,{
-	onChange = function(self)
-		self:UpdateBlurProperties()
-	end,
-    min = 0,
-    max = 10
-})
-Component:RegisterMember("BloomThreshold",udm.TYPE_FLOAT,1.0,{
 	onChange = function(self)
 		self:UpdateBlurProperties()
 	end,
@@ -44,23 +37,22 @@ end
 function Component:OnRemove()
     local renderer = self:GetRenderer()
     if(renderer == nil) then return end
-    renderer:GetEntity():RemoveComponent(ents.COMPONENT_RENDERER_PP_BLOOM)
+    renderer:GetEntity():RemoveComponent(ents.COMPONENT_RENDERER_PP_GLOW)
 end
 
 function Component:UpdateBlurProperties()
     local renderer = self:GetRenderer()
     if(renderer == nil) then return end
-    local ppBloom = renderer:GetEntity():GetComponent(ents.COMPONENT_RENDERER_PP_BLOOM)
-    if(ppBloom == nil) then return end
-    ppBloom:SetBlurRadius(self:GetBlurRadius())
-    ppBloom:SetBlurSigma(self:GetBlurSigma())
-    ppBloom:SetBloomThreshold(self:GetBloomThreshold())
-    ppBloom:SetBlurAmount(self:GetBlurAmount())
+    local ppGlow = renderer:GetEntity():GetComponent(ents.COMPONENT_RENDERER_PP_GLOW)
+    if(ppGlow == nil) then return end
+    ppGlow:SetBlurRadius(self:GetBlurRadius())
+    ppGlow:SetBlurSigma(self:GetBlurSigma())
+    ppGlow:SetBlurAmount(self:GetBlurAmount())
 end
 
 function Component:OnEntitySpawn()
     local renderer = self:GetRenderer()
     if(renderer == nil) then return end
-    renderer:GetEntity():AddComponent(ents.COMPONENT_RENDERER_PP_BLOOM)
+    renderer:GetEntity():AddComponent(ents.COMPONENT_RENDERER_PP_GLOW)
 end
-ents.COMPONENT_PFM_BLOOM = ents.register_component("pfm_bloom",Component)
+ents.COMPONENT_PFM_GLOW = ents.register_component("pfm_glow",Component)
