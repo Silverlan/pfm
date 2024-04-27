195b89aea11d508aacfe19b129f39b6fc5c5e677
Silverlan <silverlann@gmail.com>
2024-04-27 06:31:16 +0200

    feat: enable depth test for pfm_gizmo shader

--- lua/shaders/pfm/pfm_gizmo.lua
+++ lua/shaders/pfm/pfm_gizmo.lua
@@ -6,10 +6,9 @@ function shader.PFMFlat:__init()
 	shader.BaseTexturedLit3D.__init(self)
 end
 function shader.PFMFlat:InitializePipeline(pipelineInfo, pipelineIdx)
-	shader.BaseGraphics.InitializePipeline(self, pipelineInfo, pipelineIdx)
+	shader.BaseTexturedLit3D.InitializePipeline(self, pipelineInfo, pipelineIdx)
 
-	-- TODO
-	-- pipelineInfo:SetDepthBiasEnabled(true)
-	-- pipelineInfo:SetDepthBiasConstantFactor(-1)
+	pipelineInfo:SetDepthTestEnabled(true)
+	pipelineInfo:SetDepthWritesEnabled(true)
 end
 shader.register("pfm_gizmo", shader.PFMFlat)
