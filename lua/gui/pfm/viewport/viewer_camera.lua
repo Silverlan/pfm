8a9a9f581c7de3086de402a7ff9984dd86bc093b
Silverlan <silverlann@gmail.com>
2024-07-14 09:27:57 +0200

    fix(viewer_camera): fix camera pose not being updated when changing pivot distance using scroll wheel

--- lua/gui/pfm/viewport/viewer_camera.lua
+++ lua/gui/pfm/viewport/viewer_camera.lua
@@ -24,6 +24,7 @@ function Element:ScrollCallback(xoffset, yoffset)
 	local workCameraC = (vc ~= nil) and vc:GetEntity():GetComponent(ents.COMPONENT_PFM_WORK_CAMERA)
 	if workCameraC ~= nil then
 		pfm.tag_render_scene_as_dirty()
+		self:UpdateViewerCameraPose()
 		workCameraC:SetPivotDistance(math.max(workCameraC:GetPivotDistance() - yoffset * 10.0, 0.0))
 	end
 end
