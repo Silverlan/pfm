57233a578fb2d44bbee7d02f71aa8b021343bcdb
Silverlan <silverlan@pragma-engine.com>
2026-02-27 20:55:54 +0100

    feat: re-add code file

---
+++ lua/gui/base_clip.lua
@@ -0,0 +1,66 @@
+-- SPDX-FileCopyrightText: (c) 2019 Silverlan <opensource@pragma-engine.com>
+-- SPDX-License-Identifier: MIT
+
+include("/gui/selection/outline.lua")
+
+util.register_class("gui.BaseClip", gui.Base)
+
+gui.BaseClip.TITLE_COLOR = Color.White
+gui.BaseClip.TITLE_COLOR_SELECTED = Color(63, 53, 20)
+gui.BaseClip.OUTLINE_COLOR = Color(182, 182, 182)
+
+function gui.BaseClip:__init()
+	gui.Base.__init(self)
+end
+function gui.BaseClip:OnInitialize()
+	gui.Base.OnInitialize(self)
+
+	local w = 128
+	local h = 23
+	self:SetSize(w, h)
+
+	self.m_bg = gui.create("WIRect", self, 0, 0, w, h, 0, 0, 1, 1)
+	self.m_bg:SetName("background")
+	self.m_bg:SetColor(Color(121, 121, 121))
+
+	self.m_bgOutline = gui.create("WIOutlinedRect", self, 0, 0, w, h, 0, 0, 1, 1)
+	self.m_bgOutline:SetName("outline")
+	self.m_bgOutline:SetColor(gui.BaseClip.OUTLINE_COLOR)
+	self.m_bgOutline:SetOutlineWidth(2)
+
+	self.m_selection = gui.create("selection_outline", self, 0, 0, self:GetWidth(), self:GetHeight(), 0, 0, 1, 1)
+	self.m_selection:SetVisible(false)
+	self.m_selection:SetZPos(1)
+
+	self.m_text = gui.create("WIText", self, 4, 0, w - 8, 14, 0, 0, 1, 0)
+	self.m_text:SetFont("pfm_small")
+	self.m_text:SetColor(gui.BaseClip.TITLE_COLOR)
+	self.m_text:SetZPos(2)
+
+	self:SetSelected(false)
+end
+function gui.BaseClip:SetText(text)
+	if util.is_valid(self.m_text) == false then
+		return
+	end
+	self.m_text:SetText(text)
+	self.m_text:SizeToContents()
+end
+function gui.BaseClip:IsSelected()
+	return self.m_selected
+end
+function gui.BaseClip:SetSelected(selected)
+	if selected == self:IsSelected() then
+		return
+	end
+	self.m_selected = selected
+	self.m_selection:SetVisible(selected)
+	if self.m_text:IsValid() then
+		self.m_text:SetColor(selected and Color(63, 53, 20) or Color.White)
+	end
+	if selected then
+		self:CallCallbacks("OnSelected")
+	else
+		self:CallCallbacks("OnDeselected")
+	end
+end
